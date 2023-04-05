c221fdc0a4747aba31a34162634b9951edc1ab66
tarrasjose <104996954+tarrasjose@users.noreply.github.com>
2023-04-05 16:03:12 -0300

    posicao scroll animacao

--- js/script.js
+++ js/script.js
@@ -1,6 +1,5 @@
 import ScrollSuave from './modules/scroll-suave.js';
 
-import initAnimacaoScroll from './modules/scroll-animacao.js';
 import initAccordion from './modules/accordion.js';
 import initTabNav from './modules/tabnav.js';
 import initModal from './modules/modal.js';
@@ -10,6 +9,7 @@ import initMenuMobile from './modules/menu-mobile.js';
 import initFuncionamento from './modules/funcionamento.js';
 import initFetchAnimais from './modules/fetch-animais.js';
 import initFetchBitcoin from './modules/fetch-bitcoin.js';
+import initAnimacaoScroll from './modules/scroll-animacao.js';
 
 // const options = {
 //   behavior: 'smooth',
