a0fdc676be6151c5c2fc54ee7340dc4c1e1031ec
tarrasjose <104996954+tarrasjose@users.noreply.github.com>
2023-04-11 13:00:31 -0300

    menu mobile refatorado

--- js/script.js
+++ js/script.js
@@ -6,8 +6,8 @@ import Tooltip from './modules/tooltip.js';
 import fetchAnimais from './modules/fetch-animais.js';
 import fetchBitcoin from './modules/fetch-bitcoin.js';
 import DropdownMenu from './modules/dropdown-menu.js';
+import MenuMobile from './modules/menu-mobile.js';
 
-import initMenuMobile from './modules/menu-mobile.js';
 import initFuncionamento from './modules/funcionamento.js';
 import ScrollAnima from './modules/scroll-anima.js';
 
@@ -41,7 +41,9 @@ scrollAnima.init();
 const dropdownMenu = new DropdownMenu('[data-dropdown]');
 dropdownMenu.init();
 
-initMenuMobile();
+const menuMobile = new MenuMobile('[data-menu="button"]', '[data-menu="list"]');
+menuMobile.init();
+
 initFuncionamento();
 
 fetchAnimais('../../animaisapi.json', '.numeros-grid');
